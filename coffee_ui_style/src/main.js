eafed054a17ccd5a66d782e008285bb0bddf79ba
Karin1412 <100667390+Karin1412@users.noreply.github.com>
2024-06-07 21:45:02 +0700

    update header

--- coffee_ui_style/src/main.js
+++ coffee_ui_style/src/main.js
@@ -5,9 +5,14 @@ const topMenuIcon = document.getElementById('karin-top-menu-icon')
 document.addEventListener('click', (e) => {
     if(topMenuIcon.contains(e.target)){
         //click inside
+        topMenu.classList.toggle('karin-top-menu-expanded')
         topMenu.classList.toggle('hidden')
     }else{
         //click outside
-
+        if(topMenu.classList.contains('karin-top-menu-expanded')){
+            topMenu.classList.remove('karin-top-menu-expanded')
+            topMenu.classList.add('hidden')
+        }
+      
     }
 })
--- coffee_ui_style/tailwind.config.js
+++ coffee_ui_style/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes:{
+        slideDown:{
+          '0%':{transform: 'translateY(-100%)'},
+          '100%':{transform: 'translateY(0)'},
+        }
+      },
+      animation:{
+        slideDown: 'slideDown .7s ease-in-out',
+      }
+    },
   },
   plugins: [],
 }
